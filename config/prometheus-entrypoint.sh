be488e95a4a8bf4554cde8b7ec9067311ef05827
soralis <soralis.nem@gmail.com>
2025-11-21 00:28:04 +0900

    add docker host

--- config/prometheus-entrypoint.sh
+++ config/prometheus-entrypoint.sh
@@ -1,6 +1,51 @@
 #!/bin/sh
 set -eu
 
+# Resolve host-gateway placeholder to the Docker gateway address.
+resolve_target() {
+  value="$1"
+
+  case "${value}" in
+    host-gateway|host.docker.internal)
+      if [ -z "${HOST_GATEWAY_IP:-}" ]; then
+        HOST_GATEWAY_IP="$(ip -4 route show default | awk 'NR==1 {print $3}')"
+      fi
+      printf '%s' "${HOST_GATEWAY_IP}"
+      ;;
+    *)
+      printf '%s' "${value}"
+      ;;
+  esac
+}
+
+add_target() {
+  ip="$1"
+  label="$2"
+
+  if [ -z "${ip}" ]; then
+    return
+  fi
+
+  resolved_ip="$(resolve_target "${ip}")"
+
+  if [ -z "${resolved_ip}" ]; then
+    return
+  fi
+
+  if [ -n "${JSON_COMMA:-}" ]; then
+    printf '%s' "${JSON_COMMA}"
+  fi
+
+  cat <<EOF
+  {
+    "targets": ["${resolved_ip}:19999"],
+    "labels": {"node": "${label}"}
+  }
+EOF
+
+  JSON_COMMA=',\n'
+}
+
 # Build file-based service discovery targets based on provided IPs.
 SD_DIR=/tmp/prometheus-file-sd
 SD_FILE="${SD_DIR}/netdata.json"
@@ -8,18 +53,9 @@ mkdir -p "${SD_DIR}"
 
 {
   printf '[\n'
-  sep=''
-
-  if [ -n "${R7525_01_NETDATA_IP:-}" ]; then
-    printf '  %s{\n    "targets": ["%s:19999"],\n    "labels": {"node": "R7525_01"}\n  }\n' "$sep" "${R7525_01_NETDATA_IP}"
-    sep=','
-  fi
-
-  if [ -n "${RX2540M4_01_NETDATA_IP:-}" ]; then
-    printf '  %s{\n    "targets": ["%s:19999"],\n    "labels": {"node": "RX2540M4_01"}\n  }\n' "$sep" "${RX2540M4_01_NETDATA_IP}"
-    sep=','
-  fi
-
+  JSON_COMMA=''
+  add_target "${R7525_01_NETDATA_IP:-}" "R7525_01"
+  add_target "${RX2540M4_01_NETDATA_IP:-}" "RX2540M4_01"
   printf ']\n'
 } > "${SD_FILE}"
 
